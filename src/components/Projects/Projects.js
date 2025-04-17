839862faaa3cee9333e8760ebd7bc5fbbcb8ade4
satyabratapradhan <satyabratapradhann@gmail.com>
2025-04-17 13:19:05 +0530

    ch3

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,10 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import symon from "../../Assets/Projects/symonSayaaa.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import familyGolf from "../../Assets/Projects/familyGolf.png";
+import airbnb from "../../Assets/Projects/airbnb.png"
 
 function Projects() {
   return (
@@ -23,38 +24,38 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={symon}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Symon Saya Game"
+              description="This is a simple Simon Says game built using JavaScript, HTML, and CSS. The game generates a sequence of button flashes that the player must remember and repeat. Each correct sequence completion advances the player to the next level."
+              ghLink="https://github.com/satyabratapradhan01/Simon-Says-Game"
+              demoLink="https://simon-says-game-mauve.vercel.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={familyGolf}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Sidcup Family Golf"
+              description="An engaging, animated, and interactive website for Sidcup Family Golf, built with HTML, CSS, JavaScript, and GSAP animation library. This project replicates a modern and visually striking golf facility website with dynamic cursors, scroll-based animations, and responsive UI."
+              ghLink="https://github.com/satyabratapradhan01/Animation-website"
+              demoLink="https://animation-website-olive.vercel.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={airbnb}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Airbnb"
+              description="This is an Airbnb project that allows users to authenticate, list room locations, add reviews, and perform CRUD (Create, Read, Update, Delete) operations on listings and reviews."
+              ghLink="https://github.com/satyabratapradhan01/AIRBNB-CLONE"
+              demoLink="https://airbnb-clone-6ahn.onrender.com/listings"              
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
@@ -86,7 +87,7 @@ function Projects() {
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
-          </Col>
+          </Col> */}
         </Row>
       </Container>
     </Container>
